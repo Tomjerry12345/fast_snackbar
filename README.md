bbd3303479efeb96ba6a94e01a3d008a497f13f8
Tomjerry <andrialfiandi46@gmail.com>
2023-12-02 05:11:22 +0800

    fix: a package

---
+++ README.md
@@ -0,0 +1,59 @@
+<!--
+This README describes the package. If you publish this package to pub.dev,
+this README's contents appear on the landing page for your package.
+
+For information about how to write a good package README, see the guide for
+[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
+
+For general information about developing packages, see the Dart guide for
+[creating packages](https://dart.dev/guides/libraries/create-library-packages)
+and the Flutter guide for
+[developing packages and plugins](https://flutter.dev/developing-packages).
+-->
+
+## Description
+fast_snackbar: Simplifies snack bar presentation in Flutter. The FastSnackbar extension on BuildContext enables displaying snack bars without explicit BuildContext. Key features include displaying snack bars in varied colors like green (success), red (error), yellow (warning), as well as support for customizable hexadecimal color codes and Colors widget to match user preferences.
+
+## Features
+
+In your pubspec.yaml
+```dart
+dependencies:
+  firebase_utilitas: ^latest-version
+```
+
+## Usage
+
+```dart
+ElevatedButton(
+ child: Text('succes snackbar'),
+ onPressed: () {
+   // You can use TypeFastSnackbar.success | TypeFastSnackbar.error | TypeFastSnackbar.warning  
+   context.showFastSnackbar("sukses", color: TypeFastSnackbar.success);
+ },
+),
+```
+
+Or
+
+```dart
+ElevatedButton(
+ child: Text('show snackbar'),
+ onPressed: () {
+    // You can use Colors widget
+   context.showFastSnackbar("sukses", color: Colors.red);
+ },
+),
+```
+
+Or
+
+```dart
+ElevatedButton(
+ child: Text('show snackbar'),
+ onPressed: () {
+    // You can use Colors hexadesimal
+   context.showFastSnackbar("sukses", color: "#000");
+ },
+),
+```
--- lib/fast_snackbar.dart
+++ lib/fast_snackbar.dart
@@ -2,33 +2,54 @@ library fast_snackbar;
 
 import 'package:flutter/material.dart';
 
-enum TypeFastSnackbar { success, error, hex() }
+enum TypeFastSnackbar { success, error, warning }
 
 Color _hexToColor(String code) {
-  return Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
-}
+  String hexCode = code.replaceAll('#', '');
 
-final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
-    GlobalKey<ScaffoldMessengerState>();
-
-void showFastSnackbar(BuildContext context, text,
-    {dynamic type = TypeFastSnackbar.success}) {
-  if (text == null) return;
-
-  Color? c;
-
-  if (type == TypeFastSnackbar.success) {
-    c = Colors.green;
-  } else if (type == TypeFastSnackbar.error) {
-    c = Colors.red;
-  } else if (type.characters.first == "#") {
-    c = _hexToColor(type);
-  } else {
-    c = type;
+  if (hexCode.length == 3) {
+    hexCode =
+        '${hexCode[0]}${hexCode[0]}${hexCode[1]}${hexCode[1]}${hexCode[2]}${hexCode[2]}';
   }
 
-  final snackBar = SnackBar(content: Text("$text"), backgroundColor: c);
+  if (hexCode.length == 6) {
+    hexCode = 'FF$hexCode';
+  }
 
-  // scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  return Color(int.parse(hexCode, radix: 16));
+}
+
+extension FastSnackbar on BuildContext {
+  /// [type] : String hex | TypeFastSnackbar | Color color
+  /// default : TypeFastSnackbar.success
+  void showFastSnackbar(String text,
+      {dynamic color = TypeFastSnackbar.success}) {
+    ScaffoldMessenger.of(this).removeCurrentSnackBar();
+
+    Color? c;
+
+    if (color is TypeFastSnackbar) {
+      if (color == TypeFastSnackbar.success) {
+        c = Colors.green;
+      } else if (color == TypeFastSnackbar.error) {
+        c = Colors.red;
+      } else if (color == TypeFastSnackbar.warning) {
+        c = Colors.yellow;
+      }
+    } else if (color is String) {
+      if (color.characters.first == "#") {
+        c = _hexToColor(color);
+      } else {
+        throw Exception("Type hexa not first #");
+      }
+    } else if (color is Color) {
+      c = color;
+    } else {
+      throw Exception("Type is not support");
+    }
+
+    final snackBar = SnackBar(content: Text("$text"), backgroundColor: c);
+
+    ScaffoldMessenger.of(this).showSnackBar(snackBar);
+  }
 }
